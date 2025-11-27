1e3c0d6c3a654eb18709605061dd522d82b60a1c
Weak ⚡️ <32837263+kirteria@users.noreply.github.com>
2025-11-27 09:21:52 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   other: {
     "fc:frame": JSON.stringify({
       version: "next",
-      imageUrl: "https://skullines.xyz/image.gif",
+      imageUrl: "https://skullines.xyz/skullines.gif",
       button: {
         title: "Mint Skull",
         action: {
