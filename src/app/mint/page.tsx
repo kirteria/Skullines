e14a258eb3308c64867b42501cf80c7f66771c9c
Weak ⚡️ <32837263+kirteria@users.noreply.github.com>
2025-11-28 05:46:39 +0700

    Update page.tsx

--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -69,7 +69,7 @@ export default function MintPage() {
       const cid = process.env.NEXT_PUBLIC_NFT_CID!
       const appUrl = process.env.NEXT_PUBLIC_APP_URL!
       const collectionName = process.env.NEXT_PUBLIC_NFT_NAME!
-      const nftImageUrl = `https://${cid}.ipfs.dweb.link/${lastTokenId}.png`
+      const nftImageUrl = `https://nftstorage.link/ipfs/${cid}/${lastTokenId}.png`
 
       await sdk.actions.composeCast({
         text: `Just minted my ${collectionName} 💜\n\u200B\nGet yours now 💀🔥`,
