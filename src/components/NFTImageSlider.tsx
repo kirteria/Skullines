9fb6fb629cf25198e1ab1a63687a0c8efb1ad133
Weak ⚡️ <32837263+kirteria@users.noreply.github.com>
2025-11-28 07:02:40 +0700

    Update NFTImageSlider.tsx

--- src/components/NFTImageSlider.tsx
+++ src/components/NFTImageSlider.tsx
@@ -2,11 +2,14 @@
 
 import Image from 'next/image'
 
-export function NFTImageSlider({ className = '' }) {
+interface NFTImageSliderProps {
+  className?: string
+  mintPrice?: string
+}
+
+export function NFTImageSlider({ className = '', mintPrice }: NFTImageSliderProps) {
   return (
-    <div
-      className={`aspect-square bg-[#101010] rounded-2xl overflow-hidden relative ${className}`}
-    >
+    <div className={`aspect-square bg-[#101010] rounded-2xl overflow-hidden relative ${className}`}>
       <Image
         src="/preview.gif"
         alt="nft"
@@ -14,6 +17,11 @@ export function NFTImageSlider({ className = '' }) {
         sizes="280px"
         className="object-cover absolute inset-0"
       />
+      {mintPrice && (
+        <div className="absolute top-2 left-2 bg-black bg-opacity-60 px-3 py-1 rounded-full text-white text-sm font-bold shadow-md backdrop-blur-sm">
+          {mintPrice}
+        </div>
+      )}
     </div>
   )
 }
