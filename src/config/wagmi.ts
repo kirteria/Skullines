2b2d0db12e0d6700d2f26ff7febb2cc2e485f5c4
Weak ⚡️ <32837263+kirteria@users.noreply.github.com>
2025-11-29 20:35:17 +0700

    Update wagmi.ts

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -2,17 +2,17 @@
 'use client'
 
 import { http, createConfig } from 'wagmi'
-import { base } from 'wagmi/chains'
+import { baseSepolia } from 'wagmi/chains'
 import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector'
 
 const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL
 
 export const wagmiConfig = createConfig({
-chains: [base],
+chains: [baseSepolia],
 connectors: [farcasterMiniApp()],
 multiInjectedProviderDiscovery: false,
 transports: {
-[base.id]: http(RPC_URL),
+[baseSepolia.id]: http(RPC_URL),
 },
 ssr: true,
 })
