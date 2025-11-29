22e537cd2c4ee946053228c299f9b33b91f1bb43
Weak ⚡️ <32837263+kirteria@users.noreply.github.com>
2025-11-29 15:28:45 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function HomePage() {
       style={{ backgroundColor: '#101010' }}
     >
       <div className="max-w-sm w-full flex flex-col items-center text-center space-y-4">
-        <div className="w-[200px] h-[200px] rounded-2xl overflow-hidden shadow-lg">
+        <div className="w-[250px] h-[250px] rounded-2xl overflow-hidden shadow-lg">
           <img src="/skull.png" alt="Logo" className="w-full h-full object-cover" />
         </div>
 
