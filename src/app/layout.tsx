fa111c3497077bf6bacb40c648e0b6dde420e67f
Weak ⚡️ <32837263+kirteria@users.noreply.github.com>
2025-11-29 16:48:48 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <FarcasterWrapper>{children}</FarcasterWrapper>
+          {children}
         </Providers>
       </body>
     </html>
